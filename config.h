72ad879af539b81e8849d97f8faa29707759d344
Ross Fletcher <ross.fletcher@korelogic.co.uk>
2020-09-12 21:20:22 +0100

    add screen size and fix font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar           = 1;     /* 0 means no bar */
 static const int topbar            = 1;     /* 0 means bottom bar */
-static const char *fonts[] = { "DroidSansMono:size=16", };
+static const char *fonts[] = { "DroidSansMono:size=12", };
 static const char dmenufont[] = "status-18";
 
 static const char col_gray1[]       = "#2e3440";
@@ -63,10 +63,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *lockcmd[]  = { "xscreensaver-command", "-lock", NULL };
+static const char *lockcmd[]  = { "slock", NULL };
 static const char *browser[] = { "chromium", NULL };
 static const char *bup[] = { "bright", "up", NULL };
 static const char *bdown[] = { "bright", "down", NULL };
+static const char *screenie[] = { "screenie", NULL };
+static const char *screenieclip[] = { "screenie", "clip", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -76,6 +78,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ 0,                            XF86MonBrightnessDown,     spawn,          {.v = bdown } },
 	{ 0,                            XF86MonBrightnessUp,     spawn,          {.v = bup } },
+	{ MODKEY,                       XK_Print,     spawn,          {.v =screenie } },
+	{ MODKEY|ShiftMask,             XK_Print,     spawn,          {.v =screenieclip } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
